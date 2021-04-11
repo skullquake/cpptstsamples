cde32f6c6c6870e6af7408eff542eae4ba724a39
skullquake <ockert8080@gmail.com>
2021-04-11 08:19:02 -0700

    .

--- src/template/scr/builddos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-TOOLCHAINDIR=/usr/local/djgpp/bin/ PREFIX=i586-pc-msdosdjgpp POSTFIX= make
--- src/template/scr/buildnix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TOOLCHAINDIR=/usr/bin/ PREFIX=x86_64-linux-gnu POSTFIX= make
-TOOLCHAINDIR=/usr/bin/ PREFIX=x86_64-linux-gnu POSTFIX=7 make
-
